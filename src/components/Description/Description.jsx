568fb88dc413a43f33089ba360400ddb957fac1d
iryshanykkolaevna <141450700+iryshanykkolaevna@users.noreply.github.com>
2024-03-03 13:29:21 +0200

    finish

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,17 +1,13 @@
-import { GiCupcake } from "react-icons/gi";
+import css from './Description.module.css'
 
 const Description = () => {
-  return (
-    <div>
-      <h1>
-        Sip Happens Café <GiCupcake />
-      </h1>
-      <p>
-        Please leave your feedback about our service by selecting one of the
-        options below.
-      </p>
-    </div>
-  );
-};
 
-export default Description;
+    return (
+      <div className={css.descr}>
+        <h1 className={css.descrTitle}>Sip Happens Café</h1>
+        <p>Please leave your feedback about our service by selecting one of the options below.</p>
+      </div>
+    )
+  };
+
+export default Description;  
